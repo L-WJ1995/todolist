f1bd8f92cb85a24738a486852ca663eddc0e69a4
L-WJ <louwangjun_rf@foxmail.com>
2018-09-06 10:13:37 +0800

    readme

---
+++ README.md
@@ -0,0 +1,23 @@
+#   实现todolist
+### 本案例主要在todolist交互,主要交互有提交问题、删除提问、清除提问、修改问题、选择问题、选择问题不可见、非选择问题不可见等.而每个交互事件又有许多细节需要处理, 例如左上角箭头颜色改变、下拉菜单消失等。主要监听Enter事件、单双击事件。  
+[效果展示](https://htmlpreview.github.io/?https://github.com/L-WJ1995/todolist/blob/master/todolist.html)  
+[原页面](http://todomvc.com/examples/vue/)  
+  
+1. 添加问题：document事件代理监听Enter事件,重复克隆`document.cloneNode`已确定好的DOM结构,同时增加问题数量,克隆来的节点额外添加clone_类名。此处有个细节,如果当前选项为"Completed",那么新添加的元素应都是不可见的。如果是第一次添加问题,那么应该出现下拉菜单。     
+  
+2. 选择问题：依然是事件代理监听(.tick)的单机事件,如果.tick元素还没被选择,则添加一个类名tick_on,css会覆盖,如果单机元素已包含tick_on类名(即反向选择),则删除类名。此操作依然有其他联动项,如当前选项为"Completed"时,反向选择的元素应该隐藏,同时问题数加一如果为"Active"则选择的元素隐藏,同时问题数减一。  
+  
+3. 删除问题：document事件代理监听删除按钮的单机事件,同时此处自定义了一个监听问题数量变动的事件(`new Event()`/`document.dispatchEvent()`),一旦删除某个问题后问题数量为0且不存在选择的问题,则下单菜单消失,此事件还监听问题数量为0时,提问框小箭头的颜色的改变与否。  
+  
+4. 清除问题：document事件代理监听Clear completed按钮的单机事件,清除问题列表中打了勾的项目(打勾项添加了自定义类名tick_on,主要根据这个来甄别),同时此处依然监听自定义事件。  
+  
+5. 修改问题：document事件代理监听问题元素(.clone_.lastChild)的双击事件,触发事件后 隐藏该项父元素内所有的子元素,同时添加一个input元素,值为原span的innerHTML,此处又有一个document事件代理的单击事件和Enter事件,如果单击事件触发且来源元素不是input元素或者Enter事件来源与input元素,则原来隐藏的元素恢复显示,同时原span的innerHTML设置为input的value,最后隐藏input元素。  
+  
+6. 选择问题不可见/非选择问题不可见：同样是监听单机事件,而后根据当前选项的"All"/"Active"/"Completed"状态决定隐藏还是显示问题元素。  
+  
+7. 其实单个项实现并不难,主要在于每个交互产生的一系列的联动比较多,需要考虑周全。
+  
+
+
+
+
--- todolist.js
+++ todolist.js
@@ -1,3 +1,145 @@
+let parentNode = document.body.querySelector(".todos")
+let arrows = document.body.querySelector(".input span")
+let tailNode = parentNode.querySelector(".tail")
+let tailNode_A = tailNode.querySelectorAll("A")
+let A_status = "all", arrows_status = false
+
+
+
+let event = new Event('numchange')
+document.addEventListener('numchange', () => {
+  if (/^[0-9]*/.exec(tailNode.firstElementChild.textContent)[0] !== "0") arrows.style.color = "rgba(0, 0, 0, .2)"
+  else {
+    arrows.style.color = "black"
+    if (!document.body.querySelector(".clone_")) {
+      arrows.style.visibility = "hidden"
+      tailNode.style.display = "none"
+    }
+  }
+});
+
+
+
+
+
 document.addEventListener("keydown", (e) =>  {
-  if (e.target.className === "keydown" && e.keyCode === 13)
+  if (e.target.className === "keydown" && e.keyCode === 13 && e.target.value !== "") {
+    let node = document.body.querySelector(".clone").cloneNode(true)
+    node.classList.add("clone_")
+    node.lastChild.innerHTML = parentNode.querySelector(".keydown").value
+    node.style.display = A_status === "completed" ? "none" : "flex"
+    parentNode.appendChild(node)
+    e.target.value = ""
+    arrows.style.visibility = "visible"
+    tail()
+  }
+  if (e.keyCode === 13) reinput(e.target)
 })
+
+document.onclick = (e) => {
+  if (e.target.className !== "reinput") reinput()
+  if (e.target === arrows) tick("arrows")
+  if (e.target.classList.contains("tick")) tick(e.target)
+  if (tailNode.contains(e.target)) tail(e.target)
+  if (e.target.className === "X") {  
+    if (!e.target.parentNode.querySelector(".tick_on")) tailNode.firstElementChild.textContent = /^[0-9]*/.exec(tailNode.firstElementChild.textContent)[0] - 0 - 1 + " item left"
+    parentNode.removeChild(e.target.parentNode)
+    document.dispatchEvent(event)
+  }
+}
+
+
+document.ondblclick = (e) => {
+  if (e.target === e.target.parentNode.lastChild && e.target.nodeName === "SPAN") {
+    let nodes = e.target.parentNode.children
+    for (let i = 0; i < nodes.length; i++) nodes[i].style.display = "none"
+    let node = document.createElement("input")
+    node.className = "reinput"
+    node.value = e.target.innerHTML
+    e.target.parentNode.appendChild(node)
+  }
+}
+
+
+
+function reinput() {
+  let node = parentNode.querySelector(".reinput")
+  if (node) {
+    let nodes = node.parentNode.children, str = node.value
+    node.parentNode.removeChild(node)
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].style.display = ""
+        if (nodes[i] === nodes[i].parentNode.lastChild) nodes[i].innerHTML = str
+    }
+  }
+}
+
+
+function tick(node) {
+  if (node === "arrows") {
+    let nodes = document.body.querySelectorAll(".tick")
+    if (/^[0-9]*/.exec(tailNode.firstElementChild.textContent)[0] !== "0") {
+      for (let i = 0; i < nodes.length; i++) {
+        if (!nodes[i].classList.contains("tick_on") && nodes[i].parentNode.parentNode.classList.contains("clone_")) tick(nodes[i])
+      }   
+    } else for (let i = 0; i < nodes.length; i++) tick(nodes[i])
+  } else {  
+    if (node.classList.contains("tick_on")) {
+      node.classList.remove("tick_on")
+      node.parentNode.parentNode.lastChild.classList.remove("line_through")
+      tailNode.firstElementChild.textContent = /^[0-9]*/.exec(tailNode.firstElementChild.textContent)[0] - 0 + 1 + " item left"
+      clone_status()
+    } else {
+      node.classList.add("tick_on")
+      node.parentNode.parentNode.lastChild.classList.add("line_through")
+      tailNode.querySelector(".clear_completed").style.display = "inline"
+      tailNode.firstElementChild.textContent = /^[0-9]*/.exec(tailNode.firstElementChild.textContent)[0] - 0 - 1 + " item left"
+      clone_status()
+    }
+  }
+  document.dispatchEvent(event)
+}
+
+
+function tail(node = "") {
+  if (node === "") {
+    if (window.getComputedStyle(tailNode).display === "none") tailNode.style.display = "block"
+    tailNode.firstElementChild.textContent = /^[0-9]*/.exec(tailNode.firstElementChild.textContent)[0] - 0 + 1 + " item left"
+  } else {
+    if (node.nodeName === "A") {
+      for (let i = 0; i < tailNode_A.length; i++) tailNode_A[i].style["border-color"] = "rgba(255,255,255,0)"
+      node.style["border-color"] = "orange"
+      A_status = node.className
+      clone_status()
+    }
+    if (node.className === "clear_completed") {
+      let nodes = parentNode.querySelectorAll(".tick_on")
+      for (let i = 0; i < nodes.length; i++) parentNode.removeChild(nodes[i].parentNode.parentNode)
+      tailNode.querySelector(".clear_completed").style.display = "none"
+    }
+  }
+  document.dispatchEvent(event)
+}
+
+
+
+function clone_status() {
+  if (A_status === "active") {
+    let nodes = parentNode.querySelectorAll(".clone_")
+    for (let i = 0; i < nodes.length; i++) nodes[i].style.display = "flex"
+    nodes = parentNode.querySelectorAll(".tick_on")
+    for (let i = 0; i < nodes.length; i++) nodes[i].parentNode.parentNode.style.display = "none"
+  }
+  if (A_status === "all") {
+    let nodes = parentNode.querySelectorAll(".clone_")
+    for (let i = 0; i < nodes.length; i++) nodes[i].style.display = "flex"
+  }
+  if (A_status === "completed") {
+    let nodes = parentNode.querySelectorAll(".clone_")
+    for (let i = 0; i < nodes.length; i++) {
+      if (nodes[i].style.display === "flex") nodes[i].style.display = "none"
+    }
+    nodes = parentNode.querySelectorAll(".tick_on")
+    for (let i = 0; i < nodes.length; i++) nodes[i].parentNode.parentNode.style.display = "flex"
+  }
+}
